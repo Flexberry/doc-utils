24e2bcd21a60f7c7453ab107d0e35632b9477126
Semyon Volkov <svolkov@ics.perm.ru>
2019-02-14 16:57:28 +0500

    Fixed codereview

--- DocTranslate/DocTranslate/Program.cs
+++ DocTranslate/DocTranslate/Program.cs
@@ -11,8 +11,7 @@
             string workingDirectory = System.Configuration.ConfigurationManager.AppSettings["workingDirectory"];
             Console.WriteLine($"Directory: {workingDirectory}");
 
-            string[] fullFilePaths =
-                    Directory.GetFiles(workingDirectory, "*.ru.md", SearchOption.AllDirectories);
+            string[] fullFilePaths = Directory.GetFiles(workingDirectory, "*.ru.md", SearchOption.AllDirectories);
 
             ArticleTranslator articleTranslator = new ArticleTranslator();
 
@@ -24,13 +23,13 @@
                 }
                 catch (Exception exc)
                 {
-                    Console.WriteLine($"\n{fileName}\n");
+                    Console.WriteLine($"{Environment.NewLine}{fileName}{Environment.NewLine}");
                     Console.WriteLine(exc.Message);
                     Console.WriteLine(exc.StackTrace);
                 }
             }
 
-            Console.WriteLine($"Ready!!!\n Skipped old: {articleTranslator.SkippedOld}, skipped manually translated: {articleTranslator.SkippedManual}, total translated: {articleTranslator.Translated}");
+            Console.WriteLine($"Ready!!!{Environment.NewLine} Skipped old: {articleTranslator.SkippedOld}, skipped manually translated: {articleTranslator.SkippedManual}, total translated: {articleTranslator.Translated}");
         }
     }
 }
